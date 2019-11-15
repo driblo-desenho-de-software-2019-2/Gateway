19db7a0b51c244e80b0a74a793c690425035b5c1
Henrike100 <henrikem100@gmail.com>
2019-11-15 17:46:34 -0300

    Criando post de pelada

--- src/app/services/driblo.js
+++ src/app/services/driblo.js
@@ -5,3 +5,8 @@ export async function getPelada(req, res) {
     const response = await axios.get(`http://driblo:8001/pelada/${peladaId}`);
     return res.json(response.data);
 }
+
+export async function createPelada(req, res) {
+    const response = await axios.post(`http://driblo:8001/pelada`, req.body);
+    return res.json(response.data);
+}
--- src/routes.js
+++ src/routes.js
@@ -4,4 +4,14 @@ import * as Driblo from './app/services/driblo'
 const routes = new Router();
 
 routes.get('/api/pelada/:peladaId', Driblo.getPelada);
+routes.post('/api/pelada', Driblo.createPelada);
+// update
+
+
+
+
+
+
+
+
 export default routes;
